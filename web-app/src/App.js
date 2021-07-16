32ce6d3847babae12c59a6fac25c95126699ed97
Bobby <bobbykumar706584@gmail.com>
2021-07-16 15:18:28 +0530

    Navbar

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,5 +1,11 @@
+import Home from "./components/Home";
+
 function App() {
-  return <div className="App">Vachan Admin UI</div>;
+	return (
+		<div className="App">
+			<Home />
+		</div>
+	);
 }
 
 export default App;
